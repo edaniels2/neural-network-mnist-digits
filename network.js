6d376549f50e85595b1dbb18b2d295f0980b93da
Eric Daniels <edaniels@bluemoonforms.com>
2023-08-21 09:42:32 -0500

    network load params from db & training bugfixes

--- network.js
+++ network.js
@@ -1,13 +1,15 @@
 const ACTIVATION_FN = sigmoid;
+const sqlite3 = require('sqlite3');
 
 function activationFn(value) {
   return ACTIVATION_FN(value);
 }
 
-function activationFnPrime(value) {
+function activationFnDerivitive(value) {
   return ACTIVATION_FN.d(value);
 }
 
+// haven't tested this one but it's supposed to be more effective for training
 function ReLU(value) {
   return Math.max(0, value);
 }
@@ -39,11 +41,11 @@ class Neuron {
   /** @type Array<{neuron: Neuron, weight: number}> */fwdConnections = [];
 
   constructor(/** @type Array<Neuron|InputNeuron> */previousLayer) {
-    /** @type Array<{neuron: Neuron, weight: number, weightGradient: number, dZ_dW: number}> */
+    /** @type Array<{neuron: Neuron, weight: number, weightGradient: number}> */
     this.inputs = previousLayer.map(neuron => {
-      const weight = 1//neuron.isInput ? 1 : (Math.random() - 0.5) * 20;
+      const weight = Math.random() * 2 - 1;
       neuron.fwdConnections.push({ neuron: this, weight });
-      return { neuron, weight, weightGradient: 0, dZ_dW: 0 };
+      return { neuron, weight, weightGradient: 0 };
     });
   }
 
@@ -83,6 +85,8 @@ class OutputNeuron extends Neuron {
 }
 
 class Network {
+  currentTotalCost = 0;
+
   constructor(
     /** @type number */numInputs,
     /** @type number */numHiddenLayers,
@@ -93,21 +97,47 @@ class Network {
     this.inputLayer = Array(numInputs).fill(null).map(_k => new InputNeuron);
     /** @type Array<Array<Neuron>> */
     this.hiddenLayers = Array(numHiddenLayers).fill(null);
-    let layerIndex = 0;
     for (let i = 0; i < numHiddenLayers; i++) {
-      const previousLayer = layerIndex ? this.hiddenLayers[i - 1] : this.inputLayer;
+      const previousLayer = i ? this.hiddenLayers[i - 1] : this.inputLayer;
       this.hiddenLayers[i] = Array(neuronsPerLayer).fill(null).map(_k => new Neuron(previousLayer));
     }
     /** @type Array<OutputNeuron> */
     this.outputLayer = Array(outputLabels.length).fill(null).map((_k, i) => new OutputNeuron(this.hiddenLayers.at(-1), outputLabels[i]));
   }
 
-  evaluate(/** @type Array<number> */data) {
+  loadParams() {
+    /** @type sqlite3.Database */
+    const db = new sqlite3.cached.Database('./training_params.sqlite');
+    // sqlite3 hijacks the `this` binding in the callback
+    const self = this;
+    return Promise.all([
+      new Promise(resolve => db.all('SELECT * FROM weights', function (err, results) {
+        err && console.log(err)
+        results.forEach(row => {
+          const layer = row.layer >= self.hiddenLayers.length ? self.outputLayer : self.hiddenLayers[row.layer];
+          layer[row.node].inputs[row.input].weight = row.value;
+        });
+        console.log('weights loaded');
+        resolve();
+      })),
+      new Promise(resolve => db.all('SELECT * FROM biases', function (err, results) {
+        err && console.log(err)
+        results.forEach(row => {
+          const layer = row.layer >= self.hiddenLayers.length ? self.outputLayer : self.hiddenLayers[row.layer];
+          layer[row.node].bias = row.value;
+        });
+        console.log('biases loaded');
+        resolve();
+      }))
+    ]).then(() => db.close());
+  }
+
+  evaluate(/** @type Array<number> */data, /** @type Array<number> */expected) {
     if (data.length !== this.inputLayer.length) {
       throw new Error('Data length does not match input layer size');
     }
-    // set input activations - these will be in the range 0 - 255; i guess that needs to be normalized too but as long as we're using ReLU it's a freebie
-    this.inputLayer.forEach((neuron, i) => neuron.activation = data[i]);
+    // set input activations - these will be in the range 0 - 255; normalized to 0 - 1 to match the sigmoid activation fn range
+    this.inputLayer.forEach((neuron, i) => neuron.activation = data[i] / 255);
     // feed forward
     this.hiddenLayers.forEach(layer => {
       layer.forEach(neuron => neuron.updateActivation());
@@ -120,8 +150,9 @@ class Network {
       }
       return maxActive;
     });
+    this.currentTotalCost += expected ? this.cost(expected) : null;
 
-    return result;
+    return { result: {label: result.label, activation: result.activation}, fullOutput: this.outputLayer.map(n => n.activation) };
   }
 
   cost(/** @type Array<number> */expected, /** @optional @type Array<number> */input) {
@@ -131,72 +162,58 @@ class Network {
     return this.outputLayer.reduce((/** @type number */totalCost, output, i) => {
       totalCost += Math.pow(output.activation - expected[i], 2);
       return totalCost;
-    }, 0);
+    }, 0) / expected.length;
   }
 
   gradient(
-    /** @type Array<number> */expected,
-    /** @optional @type Array<number> */input,
+    /** @type Array<number> */correct,
+    /** @type Array<number> */input,
   ) {
     if (input) {
-      this.evaluate(input);
+      this.evaluate(input, correct);
     }
     this.outputLayer.forEach((node, i) => {
-      node.dCost_dOut = 2 * (node.activation - expected[i]);
-      node.dOut_dZ = activationFnPrime(node.z);
-      node.biasGradient = node.dCost_dOut * node.dOut_dZ;
+      node.dCost_dOut = 2 * (node.activation - correct[i]);
+      node.dOut_dZ = activationFnDerivitive(node.z);
+      node.biasGradient += node.dCost_dOut * node.dOut_dZ;
 
       node.inputs.forEach(input => {
-        input.weightGradient = input.neuron.activation * node.biasGradient;
+        input.weightGradient += input.neuron.activation * node.dCost_dOut * node.dOut_dZ;
       });
     });
 
     for (let layerI = this.hiddenLayers.length-1; layerI >= 0; layerI--) {
       const layer = this.hiddenLayers[layerI];
       layer.forEach(node => {
-        node.dOut_dZ = activationFnPrime(node.z);
-        let totalBiasGradient = 0;
+        node.dCost_dOut = 0;
         node.fwdConnections.forEach(fwdConnection => {
-          node.dCost_dOut = fwdConnection.weight * fwdConnection.neuron.dOut_dZ;
-          const partialBiasGradient = node.dCost_dOut * node.dOut_dZ;
-          totalBiasGradient += partialBiasGradient;
-          node.inputs.forEach(input => {
-            input.weightGradient = input.neuron.activation * partialBiasGradient * fwdConnection.neuron.dCost_dOut;
-          });
-        })
-        node.biasGradient = totalBiasGradient;
+          node.dCost_dOut += fwdConnection.neuron.dCost_dOut * fwdConnection.neuron.dOut_dZ * fwdConnection.weight;
+        });
+        node.dOut_dZ = activationFnDerivitive(node.z);
+        const partialBiasGradient = node.dCost_dOut * node.dOut_dZ;
+        node.biasGradient += partialBiasGradient;
+        node.inputs.forEach(input => {
+          input.weightGradient += input.neuron.activation * partialBiasGradient;
+        });
       });
     }
   }
 
-  updateNetwork() {
+  updateNetwork(cost = 1) {
     this.hiddenLayers.forEach(layer => {
       layer.forEach(updateNeuron);
     });
-    this.outputLayer.forEach(updateNeuron)
+    this.outputLayer.forEach(updateNeuron);
 
     function updateNeuron(/** @type Neuron */neuron) {
-      neuron.bias -= neuron.biasGradient;
-      neuron.inputs.forEach(input => input.weight -= input.weightGradient);
+      neuron.bias -= cost * neuron.biasGradient;
+      neuron.biasGradient = 0;
+      neuron.inputs.forEach(input => {
+        input.weight -= cost * input.weightGradient;
+        input.weightGradient = 0;
+      });
     }
   }
 }
 
-// const n = new Network(28*28, 2, 16, [0,1,2,3,4,5,6,7,8,9]);
-const n = new Network(2, 2, 4, ['f', 'm']);
-for (i = 0; i < 500; i++) {
-  n.gradient([1, 0], [-2, -1]);
-  n.updateNetwork();
-  n.gradient([0, 1], [25, 6]);
-  n.updateNetwork();
-  n.gradient([0, 1], [17, 4]);
-  n.updateNetwork();
-  n.gradient([1, 0], [-15, -6]);
-  n.updateNetwork();
-}
-const result = n.evaluate([-7, -3]);
-console.log(result.label, result.activation)
-
-// console.log(
-//   JSON.stringify(n, (k, v) => k === 'fwdConnections' ? undefined : v, 2)
-// );
+module.exports = { Network };
