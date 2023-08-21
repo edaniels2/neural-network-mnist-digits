b5f23640ad6b2b1054a3ddc896e020c965b1f6c3
Eric Daniels <edaniels@bluemoonforms.com>
2023-08-21 09:45:05 -0500

    client test canvas & server endpoints

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const mnist = require('./mnist');
+const { Network } = require('./network');
 
 const PORT = 1234;
 const BASE_PATH = process.cwd();
@@ -10,45 +11,71 @@ const MIME_TYPES = {
   js: 'application/javascript; charset=UTF-8',
 };
 
-const server = http.createServer(/* options,  */function (req, res) {
-  const url = req.url === '/' ? 'index.html' : req.url;
+const n = new Network(28*28, 2, 16, [0,1,2,3,4,5,6,7,8,9]);
+n.loadParams().then(startServer);
 
-  let match = url.match(/\/img\/(\d{1,4})/);
-  if (match) {
-    const data = mnist.getImage(match[1]);
-    res.writeHead(200, { 'Content-Type': 'application/octet-stream' });
-    res.write(data);
-    res.end();
-    return;
-  }
+function startServer() {
+  const server = http.createServer(/* options,  */function (req, res) {
+    const url = req.url === '/' ? 'index.html' : req.url;
 
-  match = url.match(/\/label\/(\d{1,4})/);
-  if (match) {
-    const data = mnist.getLabel(match[1]);
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write(data);
-    res.end();
-    return;
-  }
-
-  if (url === '/check') {
-    res.statusCode = 200;
-    res.end(JSON.stringify({success: true}));
-    return;
-  }
-  let filepath = path.join(BASE_PATH, url);
-  let status = 200;
-  fs.promises.access(filepath).catch(
-    function notFound() {
-      filepath = path.join(BASE_PATH, '404.html');
-      status = 404;
+    let match = url.match(/\/img\/(\d{1,4})/);
+    if (match) {
+      const data = mnist.getTrainingImage(match[1]);
+      res.writeHead(200, { 'Content-Type': 'application/octet-stream' });
+      res.write(data);
+      res.end();
+      return;
     }
-  ).finally(function send() {
-    const type = MIME_TYPES[path.extname(filepath).substring(1).toLowerCase()] || 'text/plain';
-    res.writeHead(status, { 'Content-Type': type });
-    const stream = fs.createReadStream(filepath);
-    stream.pipe(res);
-    console.log(req.httpVersion, req.method, req.url, status);
-  });
-})
-server.listen(PORT);
+
+    match = url.match(/\/label\/(\d{1,4})/);
+    if (match) {
+      const data = mnist.getTrainingLabel(match[1]);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.write(String(data));
+      res.end();
+      return;
+    }
+
+    match = url.match(/\/test\/(\d{1,4})/);
+    if (match) {
+      const data = mnist.getTestImage(match[1]);
+      const label = mnist.getTestLabel(match[1]);
+      const output = n.evaluate(data);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.write(JSON.stringify({ output, actual: label }));
+      res.end();
+      return;
+    }
+
+    if (req.method === 'POST' && url === '/evaluate') {
+      req.on('data', (/** @type Buffer */data) => {
+        const output = n.evaluate(data);
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(output));
+        res.end();
+      });
+      return;
+    }
+
+    if (url === '/check') {
+      res.statusCode = 200;
+      res.end(JSON.stringify({success: true}));
+      return;
+    }
+    let filepath = path.join(BASE_PATH, url);
+    let status = 200;
+    fs.promises.access(filepath).catch(
+      function notFound() {
+        filepath = path.join(BASE_PATH, '404.html');
+        status = 404;
+      }
+    ).finally(function send() {
+      const type = MIME_TYPES[path.extname(filepath).substring(1).toLowerCase()] || 'text/plain';
+      res.writeHead(status, { 'Content-Type': type });
+      const stream = fs.createReadStream(filepath);
+      stream.pipe(res);
+      console.log(req.httpVersion, req.method, req.url, status);
+    });
+  })
+  server.listen(PORT);
+}
