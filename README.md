96eb48cf162ed690012badbcecb65d973c77a80a
Eric Daniels <edaniels@bluemoonforms.com>
2023-08-28 15:52:09 -0500

    more training, readme, license

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-http://yann.lecun.com/exdb/mnist/
+# Digit Recognizer
+An as-basic-as-it-gets node.js implementation of an artificial neural network for classifying handwritten digits. Configurable to explore how different network configurations and training parameters effect performance.
+
+Training, testing, and gradient check modules expect the MNIST dataset files in a `mnist_dataset` directory at the project root (not included in repo; available at http://yann.lecun.com/exdb/mnist/). The server works 'out of the box'[^1] with the pre-trained params in sqlite files corresponding to a network configuration, where each number in the filename is a hidden layer with that number of neurons.
+
+[^1]: assuming node.js and sqlite3 are installed, I'll probably add a docker setup at some point to standardize that
--- meta-parameters.js
+++ meta-parameters.js
@@ -1,10 +1,10 @@
 const INPUT_SIZE = 28 * 28;
-const HIDDEN_LAYERS = [16, 16];
+const HIDDEN_LAYERS = [24, 16];
 const OUTPUTS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const TOTAL_TRAINING_SAMPLES = 60000;
 const TOTAL_TEST_SAMPLES = 10000;
-const TRAIN_ROUNDS = 200;
+const TRAIN_ROUNDS = 10; // i think the folks who know what they're doing call these epochs
 const TRAIN_BATCH_SIZE = 1000;
 const LEARN_RATE = 0.001;
 const DB_FILE = `./training_params_${HIDDEN_LAYERS.join('_')}.sqlite`;
@@ -21,8 +21,9 @@ module.exports = {
   DB_FILE,
 };
 
-// current success rate on test set for each configuration
-// 16x16: 92.49 %
-// 18x16: 86.98 % (needs retraining after bug fix)
-// 24x16: 86.69 % (needs retraining after bug fix)
-// 48x32: 88.28 % (needs retraining after bug fix)
+// current success rate on test set for some configurations I've messed with
+// 16, 16: 92.49 %
+// 18, 16: 91.96 %
+// 24, 16: 92.29 %
+// 48, 32: 93.18 %
+// 24: 90.59 % -- this one surprised me, not much drop in performance when going down to a single layer
